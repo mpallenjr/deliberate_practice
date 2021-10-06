15c96ef982662dfae073bb823b8a4e0428eef327
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-06 17:05:51 -0500

    complete challenge 3

--- skill3.rb
+++ skill3.rb
@@ -4,7 +4,16 @@
 # p first_name + " " + last_name
 
 #2
-first_name = "Mike"
-last_name = "Allen"
+# first_name = "Mike"
+# last_name = "Allen"
 
-p "#{first_name} #{last_name}"
+# p "#{first_name} #{last_name}"
+
+#3
+p "Input a word:"
+
+word = gets.chomp.downcase
+
+if word == "marco"
+  p "polo"
+end
