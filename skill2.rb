c52e4c9b5c554efdc779e9298d62b2591db32a88
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-06 16:45:51 -0500

    complete challenge 5

--- skill2.rb
+++ skill2.rb
@@ -27,10 +27,24 @@
 # end
 
 #4
-number = 9001
+# number = 9001
 
-if number > 9000
-  p 1
+# if number > 9000
+#   p 1
+# else
+#   p -1
+# end
+
+
+#5
+
+number = 21
+if number < 10
+  p 9
+elsif number < 20
+  p 19
+elsif number < 30
+  p 29
 else
   p -1
 end
