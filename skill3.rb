2a56862dd0f5fa5649e608692dc9a5d49e235eba
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-06 17:04:35 -0500

    complete challenge 2

--- skill3.rb
+++ skill3.rb
@@ -1,4 +1,10 @@
 #1
-first_name = "Fred"
-last_name = "Jackson"
-p first_name + " " + last_name
+# first_name = "Fred"
+# last_name = "Jackson"
+# p first_name + " " + last_name
+
+#2
+first_name = "Mike"
+last_name = "Allen"
+
+p "#{first_name} #{last_name}"
