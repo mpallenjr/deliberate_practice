a0509a52ab1ae9dec52c69e7de2deed56ccea6b4
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-06 17:12:32 -0500

    complete challenge 6

--- skill3.rb
+++ skill3.rb
@@ -24,8 +24,17 @@
 
 # p "My flag is " + color_1 + ", " + color_2 + ", and  " + color_3 
 
-#4
-color_1 = "red"
-color_2 = "White"
-color_3 = "blue"
-p "My flag is #{color_1}, #{color_2}, and #{color_3}."
+#5
+# color_1 = "red"
+# color_2 = "White"
+# color_3 = "blue"
+# p "My flag is #{color_1}, #{color_2}, and #{color_3}."
+
+#6
+p "enter a name:"
+name = gets.chomp.downcase
+if name == "santa"
+  p "HO HO HO"
+else
+  p "You're not santa"
+end
