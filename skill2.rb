138fe1de4974772addf85e117f1b7e2dfa598c4d
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-06 16:39:14 -0500

    complete challenge 2

--- skill2.rb
+++ skill2.rb
@@ -1,7 +1,18 @@
-#1 
-x = 5
-if x == 10
-p 0
-else
-  p -1
-end
+# #1 
+# x = 5
+# if x == 10
+# p 0
+# else
+#   p -1
+# end
+
+#2
+# number = 10
+# if number < 10
+#   p -1
+# elsif number > 10
+#   p 1
+# else
+#   p 0
+  
+# end
