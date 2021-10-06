58c6dfd298a1c5fcf0c4b307d0010f1ca6cd984d
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-06 16:51:27 -0500

    complete challenge 8

--- skill2.rb
+++ skill2.rb
@@ -63,9 +63,19 @@
 
 
 #7
-number = -1
-if number < 0 
-p 1776 
-else
-  p 1979
+# number = -1
+# if number < 0 
+# p 1776 
+# else
+#   p 1979
+# end
+
+#8 
+number = 99
+if number == 100
+  p 100
+elsif number == 99
+  p 99
+else 
+  p 0 
 end
