f51b6bf7a2b14369b4f05845c7c5674764de307d
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-07 09:07:04 -0500

    complete challenge 3

--- skill4.rb
+++ skill4.rb
@@ -5,11 +5,21 @@
 
 
 
-p "enter a number"
-number = gets.chomp.to_i
+# p "enter a number"
+# number = gets.chomp.to_i
 
-if number > 100
-  p "thats a big number"
-else
-  p "cool"
-end
+# if number > 100
+#   p "thats a big number"
+# else
+#   p "cool"
+# end
+
+
+p "enter a number"
+number_1 = gets.chomp.to_i
+p "enter a second number"
+number_2 = gets.chomp.to_i
+
+total = number_1 + number_2
+
+p total
