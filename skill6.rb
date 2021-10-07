d33f44a2f7466fa9bad0edf210ab065aa539e750
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-07 12:44:12 -0500

    complete challenge 3

--- skill6.rb
+++ skill6.rb
@@ -5,8 +5,15 @@
 
 # p words
 
-letters = ["a", "b", "y", "z"]
+# letters = ["a", "b", "y", "z"]
 
-letters[1] = 12
+# letters[1] = 12
 
-p letters
+# p letters
+
+numbers = [5, 17, 99, 9000, 8]
+index = 0
+while index < numbers.length
+  p numbers[index]
+  index += 1
+end
