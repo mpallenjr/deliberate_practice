b9923bfa83301d2fcf0e55d91143a76f2fa7add8
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-07 09:17:03 -0500

    complete challenge 10

--- skill4.rb
+++ skill4.rb
@@ -55,11 +55,20 @@
 #   p "cool"
 # end
 
-p "enter a number"
-number_1 = gets.chomp.to_i
-p "enter a second number"
-number_2 = gets.chomp.to_i
+# p "enter a number"
+# number_1 = gets.chomp.to_i
+# p "enter a second number"
+# number_2 = gets.chomp.to_i
 
-total = number_1 * number_2
+# total = number_1 * number_2
 
-p total
+# p total
+
+p "enter a word"
+word = gets.chomp
+if word.length > 5
+  p "Thats a long word"
+else
+  p "cool"
+end
+ 
