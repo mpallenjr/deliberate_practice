5772d4a8c3ad57555349102797d44c2ada2806ae
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-07 12:45:06 -0500

    complete challenge 4

--- skill6.rb
+++ skill6.rb
@@ -11,9 +11,16 @@
 
 # p letters
 
-numbers = [5, 17, 99, 9000, 8]
-index = 0
-while index < numbers.length
-  p numbers[index]
-  index += 1
-end
+# numbers = [5, 17, 99, 9000, 8]
+# index = 0
+# while index < numbers.length
+#   p numbers[index]
+#   index += 1
+# end
+
+numbers = [5]
+
+numbers << 6
+numbers << 7
+
+p numbers
