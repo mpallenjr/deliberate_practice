ce39189231c06011451eac7881efc1778075bdca
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-08 13:28:58 -0500

    complete challenge 3

--- skill9.rb
+++ skill9.rb
@@ -5,10 +5,18 @@
 # end
 # p new_arrays
 
-words = ["baseball", "swimming"]
-new_words = []
+# words = ["baseball", "swimming"]
+# new_words = []
 
-words.each do |word|
-  new_words << word.upcase
-end
-p new_words
+# words.each do |word|
+#   new_words << word.upcase
+# end
+# p new_words
+
+first_name = []
+people = [{name: "Harvey", age: 1}, {name: "Mike", age: 35}]
+
+first_name << people[0][:name]
+first_name << people[1][:name]
+
+p first_name
