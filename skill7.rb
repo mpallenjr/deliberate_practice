fb36fe0aa6e8c14a57864f7aff0a01e355c0ca96
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-08 11:44:26 -0500

    complete challenge 3

--- skill7.rb
+++ skill7.rb
@@ -5,6 +5,12 @@
 # p person[:email]
 
 
-array = [{first_name: "Mike", last_name: "Allen"},{first_name: "Jenny", last_name: "Rischall"},{first_name: "Harvey", last_name: "Allen"}]
+# array = [{first_name: "Mike", last_name: "Allen"},{first_name: "Jenny", last_name: "Rischall"},{first_name: "Harvey", last_name: "Allen"}]
 
-p array[0]
+# p array[0]
+
+menu_items = {meat: 3.00, cheese: 1.50, fries: 2.00}
+
+menu_items[:soda] = 1.00
+
+p menu_items
