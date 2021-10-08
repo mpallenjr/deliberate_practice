a7e4b24662204e3b4b4427468991e78e90f09638
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-08 11:32:52 -0500

    complete challenge 2

--- skill7.rb
+++ skill7.rb
@@ -1,5 +1,10 @@
-person = {first_name: "Mike", last_name: "Allen", email: "mikeallen@gmail.com"}
+# person = {first_name: "Mike", last_name: "Allen", email: "mikeallen@gmail.com"}
 
-p person[:first_name]
-p person[:last_name]
-p person[:email]
+# p person[:first_name]
+# p person[:last_name]
+# p person[:email]
+
+
+array = [{first_name: "Mike", last_name: "Allen"},{first_name: "Jenny", last_name: "Rischall"},{first_name: "Harvey", last_name: "Allen"}]
+
+p array[0]
