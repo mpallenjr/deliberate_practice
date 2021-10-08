d32ecc39548f8c8ae3d0b4325a3f4910c53e8e9b
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-08 13:25:17 -0500

    complete challenge 2

--- skill9.rb
+++ skill9.rb
@@ -1,6 +1,14 @@
-arrays = [1,2,3]
-new_arrays = []
-arrays.each do |array|
-  new_arrays << array * 2
+# arrays = [1,2,3]
+# new_arrays = []
+# arrays.each do |array|
+#   new_arrays << array * 2
+# end
+# p new_arrays
+
+words = ["baseball", "swimming"]
+new_words = []
+
+words.each do |word|
+  new_words << word.upcase
 end
-p new_arrays
+p new_words
